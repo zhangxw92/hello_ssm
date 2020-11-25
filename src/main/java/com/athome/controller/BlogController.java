4aa873393fef59ea925454a4be0997084d5b3a7d
zhangxw03 <zhangxw_92@163.com>
2020-11-25 22:26:28 +0800

    ssm-springMVC过滤器，form表单只支持get、post请求，因此使用HiddenRequestMethodFilter来支持，获取请求参数复习

--- src/main/java/com/athome/controller/BlogController.java
+++ src/main/java/com/athome/controller/BlogController.java
@@ -4,7 +4,10 @@ import com.athome.entity.Blog;
 import com.athome.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -12,6 +15,12 @@ import java.util.Map;
 public class BlogController {
     @Autowired
     private BlogService blogService;
+
+    @RequestMapping(value = "/testget/{id}", method = RequestMethod.GET)
+    public String login(@PathVariable("id") String id) {
+        System.out.println("----" + id);
+        return "success";
+    }
 
     @RequestMapping(value = "/getBlog")
     public String getBlog(Map<String, Object> map) {
@@ -23,6 +32,30 @@ public class BlogController {
     @RequestMapping("login")
     public String login(String username, String password) {
         System.out.println(username + "----" + password);
+        return "success";
+    }
+
+    @RequestMapping(value = "/testpost", method = RequestMethod.POST)
+    public String submit() {
+        System.out.println("Post");
+        return "success";
+    }
+
+    @RequestMapping(value = "/testpost", method = RequestMethod.PUT)
+    public String update() {
+        System.out.println("put");
+        return "success";
+    }
+
+    @RequestMapping(value = "/testpost", method = RequestMethod.DELETE)
+    public String delete() {
+        System.out.println("DELETE");
+        return "success";
+    }
+
+    @RequestMapping(value = "/submit", method = RequestMethod.POST)
+    public String submit(@RequestParam(value = "name", required = false) String username, String password) {
+        System.out.println(username + "===" + password);
         return "success";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -9,5 +9,25 @@
 <a href="getBlog">查询Blog</a>
 
 <a href="login?username=admin&password=123456">登陆</a>
+
+<%--<form action="testpost" method="POST">
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="submit" value="测试DELETE">
+</form>
+
+<form action="testpost" method="POST">
+    <input type="hidden" name="_method" value="PUT">
+    <input type="submit" value="测试put">
+</form>--%>
+
+<form action="submit" method="POST">
+    <%--
+        姓名：<input type="text" name="name"/>
+    --%>
+    密码：<input type="text" name="password"/>
+    <input type="submit" value="提交">
+</form>
+
+<a href="testget/101">测试get</a>
 </body>
 </html>
