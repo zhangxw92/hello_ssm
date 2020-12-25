9c3b8fdc1b7ca2cfd0b8892aa4305c08ddfe64e2
zhangxw03 <zhangxw_92@163.com>
2020-12-25 17:07:57 +0800

    ssm-AQS复习

---
+++ src/main/java/com/athome/lock/TestAQS.java
@@ -0,0 +1,32 @@
+package com.athome.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author zhangxw03
+ * @Dat 2020-12-25 16:00
+ * @Describe
+ */
+public class TestAQS {
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            TestAQS.test();
+        }).start();
+    }
+
+    public static void test() {
+        Lock lock = new ReentrantLock(true);
+
+        lock.lock();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        lock.unlock();
+    }
+}
