beffd852d79760087b6c0514664d5b4cfeab8b45
zhangxw03 <zhangxw_92@163.com>
2020-12-28 14:10:47 +0800

    ssm-测试oom

--- src/main/java/com/athome/lock/A.java
+++ src/main/java/com/athome/lock/A.java
@@ -6,7 +6,9 @@ public class A {
     boolean a = false;
 
     public static void main(String[] args) {
-        A a = new A();
-        System.out.println(ClassLayout.parseInstance(a).toPrintable());
+        byte[] b = new byte[50 * 1024 * 1024];
+
+//        A a = new A();
+//        System.out.println(ClassLayout.parseInstance(a).toPrintable());
     }
 }
