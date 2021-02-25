a5aef66c6552906b3cdfc63fca64cc6420219957
zhangxw03 <zhangxw_92@163.com>
2021-02-25 16:48:33 +0800

    装饰器模式

---
+++ src/main/java/com/athome/decorator2/Decorator.java
@@ -0,0 +1,10 @@
+package com.athome.decorator2;
+
+/**
+ * @Author zhangxw03
+ * @Dat 2021-02-25 16:28
+ * @Describe
+ */
+public abstract class Decorator extends Man{
+
+}
---
+++ src/main/java/com/athome/decorator2/WomanMan.java
@@ -0,0 +1,18 @@
+package com.athome.decorator2;
+
+/**
+ * @Author zhangxw03
+ * @Dat 2021-02-25 16:24
+ * @Describe
+ */
+public class WomanMan extends Man {
+//    protected String name;
+//
+//    public WomanMan(String name) {
+//        super(name);
+//    }
+
+    protected void goOut() {
+        System.out.println("要出门");
+    }
+}
